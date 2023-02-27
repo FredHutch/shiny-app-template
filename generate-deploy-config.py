b31bcc559307238b6cc461d3b193a6aefdd6d4c0
Dan Tenenbaum <dtenenba@fhcrc.org>
2023-02-27 13:53:50 -0800

    but do it correctly

--- generate-deploy-config.py
+++ generate-deploy-config.py
@@ -137,11 +137,6 @@ server {{
         proxy_ssl_server_name on;
         proxy_pass https://{fqdn};
         {auth}
-        # when app uses runApp() (not Shiny Server)
-        # you must set proxy_read_timeout (as below). See
-        # https://support.posit.co/hc/en-us/articles/213733868-Running-Shiny-Server-with-a-Proxy
-        # TODO - uncomment this line if necessary:
-        # proxy_read_timeout 20d;
     }}
 }}
 """.strip()
@@ -152,6 +147,11 @@ websockets_c = """
         proxy_http_version 1.1;
         proxy_set_header Upgrade $http_upgrade;
         proxy_set_header Connection "Upgrade";
+        # when app uses runApp() (not Shiny Server)
+        # you must set proxy_read_timeout (as below). See
+        # https://support.posit.co/hc/en-us/articles/213733868-Running-Shiny-Server-with-a-Proxy
+        # TODO - uncomment this line if necessary:
+        # proxy_read_timeout 20d;
 """.strip()
 
 auth_c = """
