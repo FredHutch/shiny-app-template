3d05fdf56384d49ea46ae32d34c0aac0303bfc8d
Dan Tenenbaum <dtenenba@fhcrc.org>
2026-01-20 12:28:38 -0800

    track github repo, formatting

--- generate-deploy-config.py
+++ generate-deploy-config.py
@@ -4,73 +4,75 @@ import argparse
 import os
 import sys
 
+
 def main(args):
-    if "_" in args['name']:
+    if "_" in args["name"]:
         print("Error: App name cannot contain underscores")
         sys.exit(1)
 
-    if "_" in args['fqdn']:
+    if "_" in args["fqdn"]:
         print("Error: FQDN cannot contain underscores")
         sys.exit(1)
 
-    if not args['fqdn'].endswith('.fredhutch.org') and \
-      not args['fqdn'].endswith('.fhcrc.org'):
+    if not args["fqdn"].endswith(".fredhutch.org") and not args["fqdn"].endswith(
+        ".fhcrc.org"
+    ):
         print("Error: FQDN must end with .fredhutch.org or .fhcrc.org")
         sys.exit(1)
 
-    print("\nCreating configs for %s\n" % args['name'])
+    print("\nCreating configs for %s\n" % args["name"])
 
-    if os.path.exists('.gitlab-ci.yml') and not args['overwrite']:
+    if os.path.exists(".gitlab-ci.yml") and not args["overwrite"]:
         print("Error: .gitlab-ci.yml already exists. Use -o to overwrite.")
         sys.exit(1)
 
-    with open('.gitlab-ci.yml', 'w') as fc:
-      fc.write(gitlab_c.format(**args))
+    with open(".gitlab-ci.yml", "w") as fc:
+        fc.write(gitlab_c.format(**args))
 
     print("Wrote .gitlab-ci.yml")
 
-
-    if os.path.exists('docker-compose.yml') and not args['overwrite']:
+    if os.path.exists("docker-compose.yml") and not args["overwrite"]:
         print("Error: docker-compose.yml already exists. Use -o to overwrite.")
         sys.exit(1)
 
     with open("docker-compose.yml", "w") as fc:
-      fc.write(docker_c.format(**args))
+        fc.write(docker_c.format(**args))
 
     print("Wrote docker-compose.yml")
 
-    if os.path.exists('.dockerignore') and not args['overwrite']:
+    if os.path.exists(".dockerignore") and not args["overwrite"]:
         print("Error: .dockerignore already exists. Use -o to overwrite.")
         sys.exit(1)
 
     with open(".dockerignore", "w") as fc:
-      fc.write(dockerignore_c)
+        fc.write(dockerignore_c)
 
     print("Wrote .dockerignore")
 
-    if args['external']:
-      if not args['no_websockets']:
-        args['websockets'] = websockets_c
-      else:
-        args['websockets'] = ""
-      if args['auth_file_path']:
-        args['auth'] = auth_c.format(**args)
-      else:
-        args['auth'] = ""
-      if args['fqdn'].endswith(".fredhutch.org"):
-        args['org'] = "fredhutch"
-      elif args['fqdn'].endswith(".fhcrc.org"):
-        args['org'] = "fhcrc"
+    if args["external"]:
+        if not args["no_websockets"]:
+            args["websockets"] = websockets_c
+        else:
+            args["websockets"] = ""
+        if args["auth_file_path"]:
+            args["auth"] = auth_c.format(**args)
+        else:
+            args["auth"] = ""
+        if args["fqdn"].endswith(".fredhutch.org"):
+            args["org"] = "fredhutch"
+        elif args["fqdn"].endswith(".fhcrc.org"):
+            args["org"] = "fhcrc"
 
-      conf_file = f"{args['fqdn']}.conf"
-      if os.path.exists(conf_file) and not args['overwrite']:
-        print(f"Error: {conf_file} already exists. Use -o to overwrite.")
-        sys.exit(1)
+        conf_file = f"{args['fqdn']}.conf"
+        if os.path.exists(conf_file) and not args["overwrite"]:
+            print(f"Error: {conf_file} already exists. Use -o to overwrite.")
+            sys.exit(1)
 
-      with open(conf_file, "w") as fc:
-        fc.write(nginx_c.format(**args))
+        with open(conf_file, "w") as fc:
+            fc.write(nginx_c.format(**args))
 
-        print(f"Wrote {conf_file}")
+            print(f"Wrote {conf_file}")
+
 
 gitlab_c = """
 variables:
@@ -135,6 +137,7 @@ services:
         condition: on-failure
       labels:
         - "org.fredhutch.app.owner={owner_email}"
+        - "org.fredhutch.github.repo={github_repo}"
         - "traefik.enable=true"
         - "traefik.http.routers.{name}.rule=Host(`{fqdn}`)"
         - "traefik.http.routers.{name}-secured.rule=Host(`{fqdn}`)"
@@ -196,36 +199,76 @@ Dockerfile*
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(
-      formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-      description='generate deploy config files for a new app',
-      epilog="""You will probably need to tweak these files manually.
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="generate deploy config files for a new app",
+        epilog="""You will probably need to tweak these files manually.
 You will need to copy the nginx config to swarm-proxy:/etc/nginx/conf.d/
 if this is an external facing app (and restart nginx).
 If app uses basic auth, you'll need to clone the app repo
-in swarm-proxy:/etc/nginx/auth_repos.""")
-    p.add_argument('-n', '--name',
-      action='store',
-      required=True,
-      help='What is the shortname of this application')
-    p.add_argument('-f', '--fqdn',
-      action='store', help="fully-qualified domain name", default="NAME.fredhutch.org")
-    p.add_argument('-p', '--port',
-      action='store', default=3838, type=int, help="port exposed by default service")
-    p.add_argument('-g', '--github-repo', default='NAME',
-      action='store', help="github repo for this app (repo name only, omit https://github.com/FredHutch/)")
-    p.add_argument('-a', '--auth-file-path',
-      action='store', help="relative path to auth file (if using basic auth). Example: system/auth",
-      )
-    p.add_argument('-o', '--overwrite', action='store_true',
-      help="overwrite existing files", default=False)
-    p.add_argument('-r', '--owner-email', action='store', help="email of owner", required=True)
-    p.add_argument('-e', '--external', action='store_true', default=False,
-      help="App will be deployed externally. Generates proxy nginx config.")
-    p.add_argument('-w', '--no-websockets', action='store_true', help='does not use websockets, only needed if EXTERNAL==True')
+in swarm-proxy:/etc/nginx/auth_repos.""",
+    )
+    p.add_argument(
+        "-n",
+        "--name",
+        action="store",
+        required=True,
+        help="What is the shortname of this application",
+    )
+    p.add_argument(
+        "-f",
+        "--fqdn",
+        action="store",
+        help="fully-qualified domain name",
+        default="NAME.fredhutch.org",
+    )
+    p.add_argument(
+        "-p",
+        "--port",
+        action="store",
+        default=3838,
+        type=int,
+        help="port exposed by default service",
+    )
+    p.add_argument(
+        "-g",
+        "--github-repo",
+        default="NAME",
+        action="store",
+        help="github repo for this app (repo name only, omit https://github.com/FredHutch/)",
+    )
+    p.add_argument(
+        "-a",
+        "--auth-file-path",
+        action="store",
+        help="relative path to auth file (if using basic auth). Example: system/auth",
+    )
+    p.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="overwrite existing files",
+        default=False,
+    )
+    p.add_argument(
+        "-r", "--owner-email", action="store", help="email of owner", required=True
+    )
+    p.add_argument(
+        "-e",
+        "--external",
+        action="store_true",
+        default=False,
+        help="App will be deployed externally. Generates proxy nginx config.",
+    )
+    p.add_argument(
+        "-w",
+        "--no-websockets",
+        action="store_true",
+        help="does not use websockets, only needed if EXTERNAL==True",
+    )
     args = p.parse_args()
-    if args.github_repo == 'NAME':
+    if args.github_repo == "NAME":
         args.github_repo = args.name
-    if args.fqdn == 'NAME.fredhutch.org':
+    if args.fqdn == "NAME.fredhutch.org":
         args.fqdn = f"{args.name}.fredhutch.org"
     # import IPython; IPython.embed();sys.exit(1)
     main(vars(args))
